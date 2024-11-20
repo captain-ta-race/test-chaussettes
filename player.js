7a868bbff1db5dc420a69c62a3b1668fe5996b20
captain-ta-race <84874037+captain-ta-race@users.noreply.github.com>
2024-11-20 12:56:08 +0100

    Update player.js

--- player.js
+++ player.js
@@ -1,5 +1,5 @@
 // Lien vers le flux RSS
-const rssFeedUrl = "https://feeds.acast.com/public/shows/6728ae32dc854c9577f0ce16"; // Utilise ton flux réel ici
+const rssFeedUrl = "https://corsproxy.io/?https://feeds.acast.com/public/shows/6728ae32dc854c9577f0ce16"; // Utilise ton flux réel ici
 const episodesContainer = document.getElementById("episodes");
 
 // Fonction principale pour charger le flux RSS
